63b084cd983fe6c68442ec38feacf49599b1de9e
ch0fo <durango.avitia@fedex.com>
2025-05-28 17:50:29 -0400

    now keeping track of awbs sent using oic tool

--- OIC_Tool/oic_tool.py
+++ OIC_Tool/oic_tool.py
@@ -7,10 +7,14 @@ import custom_classify_db
 import win32com.client
 import os
 import pandas as pd
-from typing import List
+from typing import List, Dict, Set
 import win32com
 from main_automation_programs.tools import send_email, get_query
 import datetime
+import json
+import time
+import numpy as np
+import copy
 
 class Fetch:
     queryresults: pd.DataFrame
@@ -37,6 +41,8 @@ class Fetch:
             12: 'December'
         }
         self._vbapath = r"main_automation_programs\support-files\excel-macros\oic_pivotmacro\xl\vbaProject.bin"
+        self.prev_sent_path = r"OIC_Tool\sent.json"
+        self.date_string_format = r"%d-%b-%Y"
     
     def prompt_file(self):
         print("Select file with OIC info")
@@ -141,9 +147,72 @@ class Fetch:
             if cad > 20 and coe.upper() not in self._validcoe:
                 self.queryresults.drop(i, inplace=True)
 
+    def remove_prev_sent(self) -> None:
+        """
+        Checks the previously sent awbs so they are not sent again.
+        """
+
+        #Checking if a prev sent file exists, exiting if no file to check
+        if not os.path.exists(self.prev_sent_path):
+            print("No file exists to reference previously sent awbs, one will be created shortly for future reference.")
+            return
+        
+        #If a file exists, extract awbs and remove all matches with prev sent.
+        with open(self.prev_sent_path, 'r') as f:
+            loaded_data: dict = json.load(f) #getting all rows from prev sent awbs
+        sent_awbs: Dict[int, str] = {np.int64(key): value for key, value in loaded_data.items()} #converting the loaded data keys' into ints
+        awbs_from_json: Set[int] = set(sent_awbs.keys()) #set of all awbs saved in json
+
+        #Deleting matches with the current results
+        awbs_from_results: Set[int] = set(self.queryresults['awb_nbr']) #getting awbs in results
+        duplicated_awbs: Set[int] = awbs_from_results & awbs_from_json #finding intersection
+        print(f"Found {len(duplicated_awbs)} previously sent awbs in results, dropping them.")
+        self.queryresults = self.queryresults.drop_duplicates(subset=['awb_nbr'], inplace=False) #droppping dups from awbs
+        self.queryresults = self.queryresults.set_index(keys='awb_nbr', drop= False, inplace=False) #setting awbs as index
+        self.queryresults = self.queryresults.drop(list(duplicated_awbs), inplace=False) #dropping duplicates
+
+    def add_sent_awbs(self) -> None:
+        """
+        Once previously sent awbs are removed from results, append all remaining awbs to the previously sent record.
+        """
+
+        #Adding existing sent awbs
+        awbs_to_add: List[int] = self.queryresults['awb_nbr'].tolist()
+
+        sent_awbs: Dict[int, str] = dict()
+        if os.path.exists(self.prev_sent_path):
+            with open(self.prev_sent_path, 'r') as f:
+                loaded_data: dict = json.load(f) #getting all rows from prev sent awbs
+            sent_awbs: Dict[int, str] = {int(key): value for key, value in loaded_data.items()} #converting the loaded data keys' into ints
+        #Adding new awbs
+        for awb in awbs_to_add:
+            sent_awbs[awb] = datetime.datetime.today().strftime(self.date_string_format)
+
+        #Deleting all awbs over 90 days old
+        count_old_dropped: int = 0
+        # sent_iterate: Dict[int, str] = copy.deepcopy(sent_awbs) #creating copy so we can modify original while iterating
+        for awb, date in list(sent_awbs.items()):
+            if datetime.datetime.strptime(date, self.date_string_format) < (datetime.datetime.today() - datetime.timedelta(days=90)): #converting stored date to a datetime object, then comparing if it is over 90 days old from today
+                count_old_dropped += 1
+                del sent_awbs[awb]
+        print(f"Awbs sent over 90 days ago dropped: {count_old_dropped}")
+
+        #Saving all remaining awbs back into .json file
+        with open(self.prev_sent_path, 'w') as f:
+            json.dump(obj=sent_awbs, fp=f, indent="")
+        print(f"Added {len(awbs_to_add)} awbs to previously sent record.")
+
     def pivot(self):
         path = self.get_savepath()
         self.clean_query()
+        self.remove_prev_sent()
+        self.add_sent_awbs()
+
+        #Checking if there is data left to process
+        if len(self.queryresults.index) == 0:
+            print("There are no awbs left to process, exiting and NOT creating a results file (empty results).")
+            return
+
         with pd.ExcelWriter(path = path, engine='xlsxwriter') as writer:
             self.queryresults.to_excel(writer, sheet_name='OIC', na_rep='null', index=False)
             workbook = writer.book
@@ -166,9 +235,11 @@ class Fetch:
         os.remove(path) #This is just for removing the original .xlsx document, but the .xlsm document that is acc being sent out will remain saved
 
         #Sending email with results
-        send_email(subject=f"OIC_TASK_0002 {datetime.datetime.today().strftime("%d-%b-%Y")}", files=[macro_path])
+        # send_email(subject=f"OIC_TASK_0002 {datetime.datetime.today().strftime("%d-%b-%Y")}", files=[macro_path])
         
 def main():
+
+    
     args = sys.argv
     oic = Fetch()
     oic.query(args)
